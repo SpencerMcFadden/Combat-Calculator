0d55e818eb79f86f35c9d1ad1eccbc956608ed5d
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-12-22 16:16:15 -0700

    Changing the way the result displays, along with adding in an indicator for which formula was used. Slight styling change, but all of it is pretty much temporary

--- src/CalcComponents/Calc.js
+++ src/CalcComponents/Calc.js
@@ -6,7 +6,11 @@ class Calc extends Component {
   render() {
     return (
       <div className="Calculator">
-        <h3>Testing Calc</h3>
+        <h3 id="Result-title">Type of result</h3>
+
+        <div className="Result-display">
+          <h4 id="result">...</h4>
+        </div>
 
         <div className="Calculator-input">
           <AttackDetailsForm dice={this.props.dice} formulas={this.props.formulas}/>
--- src/CalcComponents/Formula.js
+++ src/CalcComponents/Formula.js
@@ -5,40 +5,48 @@ class Formula extends Component {
     let currentFormula;
     if(this.props.formula === 'Weapon Attack') {
       currentFormula = function(hitDie, statMod, miscBonus, numberOfAttacks) {
+        document.getElementById('Result-title').innerHTML = 'Weapon Attack';
         return ((Number(hitDie) + Number(statMod) + Number(miscBonus)) * numberOfAttacks);
       }
       return (
-        <button className="Formulas" onClick={() => alert(currentFormula(this.props.hitDie, this.props.statMod, this.props.miscBonus, this.props.numberOfAttacks))}>
+        <button className="Formulas" onClick={() =>
+          document.getElementById('result').innerHTML = currentFormula(this.props.hitDie, this.props.statMod, this.props.miscBonus, this.props.numberOfAttacks)}>
           {this.props.formula}
         </button>
       )
     }
     else if(this.props.formula === 'Spell Attack Roll') {
       currentFormula = function(hitDie, miscBonus, numberOfAttacks) {
+        document.getElementById('Result-title').innerHTML = 'Spell Attack Roll';
         return ((Number(hitDie) + Number(miscBonus)) * numberOfAttacks);
       }
       return (
-        <button className="Formulas" onClick={() => alert(currentFormula(this.props.hitDie, this.props.miscBonus, this.props.numberOfAttacks))}>
+        <button className="Formulas" onClick={() =>
+          document.getElementById('result').innerHTML = currentFormula(this.props.hitDie, this.props.miscBonus, this.props.numberOfAttacks)}>
           {this.props.formula}
         </button>
       )
     }
     else if(this.props.formula === 'Spell Attack Save') {
       currentFormula = function(hitDie, numberOfDice) {
+        document.getElementById('Result-title').innerHTML = 'Spell Attack Save';
         return (Number(hitDie) * Number(numberOfDice));
       }
       return (
-        <button className="Formulas" onClick={() => alert(currentFormula(this.props.hitDie, this.props.numberOfAttacks))}>
+        <button className="Formulas" onClick={() =>
+          document.getElementById('result').innerHTML = currentFormula(this.props.hitDie, this.props.numberOfAttacks)}>
           {this.props.formula}
         </button>
       )
     }
     else if(this.props.formula === 'Healing') {
       currentFormula = function(hitDie, numberOfDice) {
+        document.getElementById('Result-title').innerHTML = 'Healing';
         return -1 * (Number(hitDie) * Number(numberOfDice));
       }
       return (
-        <button className="Formulas" onClick={() => alert(currentFormula(this.props.hitDie, this.props.numberOfAttacks))}>
+        <button className="Formulas" onClick={() =>
+          document.getElementById('result').innerHTML = currentFormula(this.props.hitDie, this.props.numberOfAttacks)}>
           {this.props.formula}
         </button>
       )
